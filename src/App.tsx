ad70f04c04158df81bf3ce6d79fc2e1502edd725
soufianeoualla <soufianeoualla0@gmail.com>
2025-05-18 21:26:12 +0100

    animated navigation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-white">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,79 @@
+import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
+import { HiDownload } from "react-icons/hi";
+
+import { AnimatePresence } from "motion/react";
+import { useState } from "react";
+import { motion } from "motion/react";
+import CustomListItem from "./CustomListItem";
+
+const links = [
+  {
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    title: "Company",
+    url: "/company",
+  },
+  {
+    title: "Products",
+    url: "/products",
+  },
+];
+const Navigation = () => {
+  const [isHovred, setIsHovred] = useState(false);
+  return (
+    <nav>
+      <ul className="flex flex-col xl:flex-row xl:items-center items-start gap-y-6 gap-x-12 text-custom-gray">
+        {links.map((link) => (
+          <CustomListItem key={link.title} label={link.title} />
+        ))}
+        <li
+          onMouseEnter={() => setIsHovred(true)}
+          onMouseLeave={() => setIsHovred(false)}
+          className="bg-light-purple h-12 flex items-center gap-x-1 rounded-3xl w-[150px]  justify-center  text-black text-lg font-medium "
+        >
+          <AnimatePresence>
+            {!isHovred && (
+              <motion.span
+                initial={{ opacity: 0, x: 10 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -10 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 40,
+                  damping: 8,
+                  mass: 1.5,
+                  duration: 0.35,
+                }}
+                className="flex items-center"
+              >
+                <HiOutlineDevicePhoneMobile className="text-2xl" />
+              </motion.span>
+            )}
+            <a href="">Download</a>
+            {isHovred && (
+              <motion.span
+                initial={{ opacity: 0, x: -10 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 10 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 40,
+                  damping: 8,
+                  mass: 1.5,
+                  duration: 0.35,
+                }}
+                className="flex items-center"
+              >
+                <HiDownload />
+              </motion.span>
+            )}
+          </AnimatePresence>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
